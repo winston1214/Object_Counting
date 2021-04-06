e2e56cc9441fed3deaaaa1b4c3f4884c1db94c99
YoungMin KIM <47775179+winston1214@users.noreply.github.com>
2021-04-06 16:42:54 +0900

    Update deep_sort.py

--- deep_sort_pytorch/deep_sort/deep_sort.py
+++ deep_sort_pytorch/deep_sort/deep_sort.py
@@ -57,7 +57,7 @@ class DeepSort(object):
             outputs.append(np.array([x1, y1, x2, y2, track_id,label_id], dtype=np.int))
         if len(outputs) > 0:
             outputs = np.stack(outputs, axis=0)
-        print(outputs)
+        
         return outputs
 
     """
